c00f226ad4f3edaf7c6fe3761ce5e9bf41e039af
sharif <61516383+sharifhsn@users.noreply.github.com>
2021-07-14 14:56:12 -0400

    code refactoring

--- chip8.py
+++ chip8.py
@@ -9,11 +9,6 @@ import time
 # rom name
 file_name = sys.argv[1]
 
-# programmer mode - optional
-prog = False
-if len(sys.argv) > 2:
-    prog = True
-
 # original chip-8, no modification
 orig = True
 amiga = False
@@ -46,6 +41,7 @@ pc = 0x200
 # stack for executing functions (LIFO) 16 levels by default
 stk = []
 
+
 # stack pointer for the index in the stack
 stkp = 0x0
 
@@ -76,7 +72,7 @@ def clear_display(win):
 
 # draw display
 def draw_display(win, display):
-    print("drawing display")
+    #print("drawing display")
     clear_display(win)
     for i in range(len(display)):
         r = display[i]
@@ -86,24 +82,6 @@ def draw_display(win, display):
                 rect = Rectangle(Point(j, i), Point(j + 1, i + 1))
                 rect.setFill("black")
                 rect.draw(win)
-    print("end draw")
-
-
-# print functions for debugging purposes
-def print_memory(memory):
-    # print mem starting at 0x200
-    i = 0x50
-    while True:
-        if memory[i] == 0 and memory[i + 1] == 0:
-            break
-        print("0x{:02X}: 0x{:02X}".format(i, memory[i]), end='\t')
-        i += 1
-
-
-def print_registers(reg):
-    for i in range(len(reg)):
-        print("v%X" % i + ": 0x%X" % reg[i], end=' ')
-    print()
 
 # load rom into memory
 with open(file_name, mode='rb') as file:
@@ -112,29 +90,16 @@ with open(file_name, mode='rb') as file:
     for i in range(len(file_content)):
         memory[j] = file_content[i]
         j += 1
-    print_memory(memory)
 
 while True:
-    # timers
+    # set cycle speed
     tick = time.time()
-    while time.time() <= tick + .0015:
+    while time.time() <= tick + 1/700:
         pass
+
     # fetch
     instr = (memory[pc] << 8) | memory[pc + 1]
-    print("0x{:03X}: 0x{:04X}".format(pc, instr), end = '\t')
     pc += 2
-
-    # debugging
-    if prog:
-        print_registers(reg)
-        print("index: 0x{:03X} | ".format(index), end='')
-        print("stk: ", end='')
-        for s in stk:
-            print("0x{:03X},".format(s), end=' ')
-        print()
-        # for r in display:
-        #     print("0x{:016x}".format(r))
-        input()
     
     # nibble information
     op = (instr >> 12) # first nibble
@@ -144,66 +109,50 @@ while True:
     nn = instr & 0xFF # second byte (8 bit number)
     nnn = instr & 0xFFF # second nibble plus second byte (12 bit memory address)
 
-
     # decode/execute
     if instr == 0x0000:
         # end of program
-        print("program is over")
         break
     elif instr == 0x00E0:
         # clear screen
-        print("clear the screen")
         for i in range(len(display)):
             display[i] = 0x0000000000000000
         clear_display(win)
     elif instr == 0x00EE:
         # pop stack
-        print("pop stack")
         pc = stk.pop()
     elif op == 0x1:
         # jump to nnn
-        print("jump to 0x%X" % nnn)
         if pc - 2 == nnn:
-            # loops on last instruction
-            print("program complete")
             break
         pc = nnn
     elif op == 0x2:
         # call nnn, push current pc to stack
-        print("push 0x{:03X} to stack and jump to 0x{:03X}".format(pc, nnn))
         stk.append(pc)
         pc = nnn
     elif op == 0x3:
         # this and next three are skips
-        print("if v{} == 0x{:02X}, then skip".format(x, nn))
         if reg[x] == nn:
             pc += 2
     elif op == 0x4:
-        print("if v{} != 0x{:02X}, then skip".format(x, nn))
         if reg[x] != nn:
             pc += 2
     elif op == 0x5 and n == 0x0:
-        print("if v{} == v{}, then skip".format(x, y))
         if reg[x] == reg[y]:
             pc += 2
     elif op == 0x9 and n == 0x0:
-        print("if v{} != v{}, then skip".format(x, y))
         if reg[x] != reg[y]:
             pc += 2
     elif op == 0x6:
         # set
-        print("set v%X" % x + " to 0x%X" % nn)
         reg[x] = nn
     elif op == 0x7:
         # add
-        print("add 0x%X" % nn + " to 0x%X" % reg[x], end=', ')
         reg[x] = (reg[x] + nn) % 0x100
-        print("sum is now 0x%X" % reg[x])
     elif op == 0x8:
         # logic
         if n == 0x0:
             # set
-            print("v{} is now set to the value in v{}".format(x, y))
             reg[x] = reg[y]
         elif n == 0x1:
             # or
@@ -242,38 +191,30 @@ while True:
             # shift right
             if orig:
                 reg[x] = reg[y]
-            print("shift v{} = 0x{:0X} right".format(x, reg[x]))
             reg[0xF] = reg[x] & 0x1
             reg[x] = reg[x] >> 1
-            print("v{} is now 0x{:0X}".format(x, reg[x]))
         elif n == 0xE:
             # shift left
             if orig:
                 reg[x] = reg[y]
-            print("shift v{} = 0x{:0X} left".format(x, reg[x]))
             reg[0xF] = reg[x] >> 7
             reg[x] = reg[x] << 1
             reg[x] = reg[x] & 0xFF
-            print("v{} is now 0x{:0X}".format(x, reg[x]))
     elif op == 0xA:
         # set index
-        print("set index to 0x{:03X} which points to 0x{:02X}".format(nnn, memory[nnn]))
         index = nnn
     elif op == 0xB:
         # offset jump
-        print("jump to {:03X} + {:02X}".format(nnn, reg[0]))
         if orig:
             pc = nnn + reg[0]
         else:
             pc = nnn + reg[x]
     elif op == 0xC:
         # random
-        print("make v{:01X} a random number &ed with {:02X}".format(x, nn))
         r = random.randint(0, nn)
         reg[x] = r & nn
     elif op == 0xD:
         # display
-        print("display instruction")
         dx = reg[x] % 0x40
         dy = 0x20 - reg[y] % 0x20
         reg[0xF] = 0
@@ -283,14 +224,12 @@ while True:
             if dy > 0x1F:
                 break
             membyt = memory[ind]
-            #print("membyt at index 0x%X" % ind + " is 0x%X" % membyt)
             dx = savx
             for j in range(0x8):
                 if dx > 0x3F:
                     break
                 dispbit = (display[dy] >> (0x3F - dx)) & 0x1
                 membit = (membyt >> (0x7 - j)) & 0x1
-                #print("dispbit is 0x{:01x} and membit is 0x{:01x}".format(dispbit, membit))
                 if membit == 1:
                     display[dy] = display[dy] ^ (0x1 << (0x3F - dx))
                     if dispbit == 1:
@@ -303,38 +242,29 @@ while True:
     elif op == 0xE and nn == 0x9E:
         # skip if key is pressed
         ind = corr.index(reg[x])
-        print(f"skip if \"{keys[ind]}\" is pressed")
         if keyboard.is_pressed(keys[ind]):
-            print(f"\"{keys[ind]}\" is indeed pressed")
             pc += 2
     elif op == 0xE and nn == 0xA1:
         # skip if key is not pressed
         ind = corr.index(reg[x])
-        print(f"skip if \"{keys[ind]}\" is not pressed")
         if not keyboard.is_pressed(keys[ind]):
-            print(f"\"{keys[ind]}\" is indeed not pressed")
             pc += 2
     elif op == 0xF:
         # various instructions
         if nn == 0x07:
             # this and two next are timers
-            print("v{:01X} is now 0x{:03X} from delay".format(x, reg[x]))
             reg[x] = int(delay)
         elif nn == 0x15:
-            print("delay is now 0x{:03X} from v{:01X}".format(reg[x], x))
             delay = reg[x]
         elif nn == 0x18:
-            print("sound is now 0x{:03X} from v{:01X}".format(reg[x], x))
             sound = reg[x]
         elif nn == 0x1E:
             # add to index
-            print("add v{:01X} = {:03X} to index".format(x, reg[x]))
             index += reg[x]
             if index > 0xFFF:
                 index %= 0x1000
                 if amiga:
                     reg[0xF] = 1
-            print("index is now {:03X}".format(index))
         elif nn == 0x0A:
             while True:
                 k = keyboard.read_key()
@@ -343,14 +273,11 @@ while True:
                 ind = keys.index(k)
                 break
             reg[x] = corr[ind]
-            print("store 0x{:03X} in v{:01X} from \"{}\" being pressed".format(reg[x], x, corr[ind]))
         elif nn == 0x29:
             # font character
             index = 0x50 + reg[x] * 0x5
-            print("index is now at the font character \"{:01X}\"".format(reg[x]))
         elif nn == 0x33:
             # binary-coded decimal conversion
-            print("put v{:01X} = {} into memory[{:03X}], in decimal".format(x, reg[x], index))
             b = reg[x]
             memory[index + 2] = b % 10
             b = int(b / 10)
@@ -359,7 +286,6 @@ while True:
             memory[index] = b % 10
         elif nn == 0x55:
             # store memory
-            print("store registers into memory at memory[{:03X}] until {:01X}".format(index, x))
             if inc:
                 for i in range(x + 1):
                     memory[index] = reg[i]
@@ -370,7 +296,6 @@ while True:
             pass
         elif nn == 0x65:
             # load memory
-            print("load registers from memory from memory[{:03X}] until {:01X}".format(index, x))
             if inc:
                 for i in range(x + 1):
                     reg[i] = memory[index]
@@ -379,8 +304,9 @@ while True:
                 for i in range(x + 1):
                     reg[i] = memory[index + i]
         else:
-            #print("false instruction loaded")
+            # incorrect instruction loaded
             exit
+    # decrement timers
     delay -= (time.time() - tick) * 60
     if delay < 0:
         delay = 0
--- chip8_debug.py
+++ chip8_debug.py
@@ -4,7 +4,7 @@ import random
 import sys
 import time
 
-# build a chip-8 emulator [all print statements commented out/deleted]
+# build a chip-8 emulator [debugging options enabled]
 
 # rom name
 file_name = sys.argv[1]
@@ -45,8 +45,6 @@ pc = 0x200
 
 # stack for executing functions (LIFO) 16 levels by default
 stk = []
-# for i in range(0x10):
-#     stk.append(0x000)
 
 # stack pointer for the index in the stack
 stkp = 0x0
@@ -65,7 +63,6 @@ for i in range(0x20):
     display.append(0x0)
 win = GraphWin(width = 64 * 16, height = 32 * 16)
 win.setCoords(0, 0, 64, 32)
-#win.getMouse()
 
 # key implementation
 keys = ['1', '2', '3', '4', 'q', 'w', 'e', 'r', 'a', 's', 'd', 'f', 'z', 'x', 'c', 'v']
@@ -79,7 +76,7 @@ def clear_display(win):
 
 # draw display
 def draw_display(win, display):
-    #print("drawing display")
+    print("drawing display")
     clear_display(win)
     for i in range(len(display)):
         r = display[i]
@@ -89,34 +86,24 @@ def draw_display(win, display):
                 rect = Rectangle(Point(j, i), Point(j + 1, i + 1))
                 rect.setFill("black")
                 rect.draw(win)
-    #print("end draw")
-# #print("drawing 1/4 left display")
-# draw_display(win, display)
-# for i in range(len(display)):
-#     display[i] = 0x0000FFFF0000FFFF
-# #print("drawing something")
-# draw_display(win, display)
-# clear_display(win)
-# draw_display(win, display)
-# for i in range(len(display)):
-#     display[i] = 0x0000999900009999
-# win.getMouse()
+    print("end draw")
+
 
 # print functions for debugging purposes
-# def print_memory(memory):
-#     # print mem starting at 0x200
-#     i = 0x50
-#     while True:
-#         if memory[i] == 0 and memory[i + 1] == 0:
-#             break
-#         #print("0x%X" % i + ": 0x%X" % memory[i], end='\t')
-#         i += 1
+def print_memory(memory):
+    # print mem starting at 0x200
+    i = 0x50
+    while True:
+        if memory[i] == 0 and memory[i + 1] == 0:
+            break
+        print("0x{:02X}: 0x{:02X}".format(i, memory[i]), end='\t')
+        i += 1
 
 
-# def print_registers(reg):
-#     for i in range(len(reg)):
-#         #print("v%X" % i + ": 0x%X" % reg[i], end=' ')
-#     #print()
+def print_registers(reg):
+    for i in range(len(reg)):
+        print("v%X" % i + ": 0x%X" % reg[i], end=' ')
+    print()
 
 # load rom into memory
 with open(file_name, mode='rb') as file:
@@ -125,29 +112,29 @@ with open(file_name, mode='rb') as file:
     for i in range(len(file_content)):
         memory[j] = file_content[i]
         j += 1
-    #print_memory(memory)
+    print_memory(memory)
 
 while True:
     # timers
     tick = time.time()
-    while time.time() <= tick + .001:
+    while time.time() <= tick + .0015:
         pass
     # fetch
     instr = (memory[pc] << 8) | memory[pc + 1]
-    #print("0x%X" % pc + ": 0x%X" % instr, end = '\t')
+    print("0x{:03X}: 0x{:04X}".format(pc, instr), end = '\t')
     pc += 2
 
     # debugging
-    #if prog:
-        #print_registers(reg)
-        #print("index: 0x{:03X} | ".format(index), end='')
-        #print("stk: ", end='')
-        #for s in stk:
-            #print("0x{:03X},".format(s), end=' ')
-        #print()
+    if prog:
+        print_registers(reg)
+        print("index: 0x{:03X} | ".format(index), end='')
+        print("stk: ", end='')
+        for s in stk:
+            print("0x{:03X},".format(s), end=' ')
+        print()
         # for r in display:
-        #     #print("0x{:016x}".format(r))
-        #input()
+        #     print("0x{:016x}".format(r))
+        input()
     
     # nibble information
     op = (instr >> 12) # first nibble
@@ -161,61 +148,62 @@ while True:
     # decode/execute
     if instr == 0x0000:
         # end of program
-        #print("program is over")
+        print("program is over")
         break
     elif instr == 0x00E0:
         # clear screen
-        #print("clear the screen")
+        print("clear the screen")
         for i in range(len(display)):
             display[i] = 0x0000000000000000
         clear_display(win)
     elif instr == 0x00EE:
         # pop stack
-        #print("pop stack")
+        print("pop stack")
         pc = stk.pop()
     elif op == 0x1:
         # jump to nnn
-        #print("jump to " + "0x%X" % nnn)
+        print("jump to 0x%X" % nnn)
         if pc - 2 == nnn:
-            #print("program complete")
+            # loops on last instruction
+            print("program complete")
             break
         pc = nnn
     elif op == 0x2:
         # call nnn, push current pc to stack
-        #print("push 0x{:03X} to stack and jump to 0x{:03X}".format(pc, nnn))
+        print("push 0x{:03X} to stack and jump to 0x{:03X}".format(pc, nnn))
         stk.append(pc)
         pc = nnn
     elif op == 0x3:
         # this and next three are skips
-        #print("if v{} == 0x{:02X}, then skip".format(x, nn))
+        print("if v{} == 0x{:02X}, then skip".format(x, nn))
         if reg[x] == nn:
             pc += 2
     elif op == 0x4:
-        #print("if v{} != 0x{:02X}, then skip".format(x, nn))
+        print("if v{} != 0x{:02X}, then skip".format(x, nn))
         if reg[x] != nn:
             pc += 2
     elif op == 0x5 and n == 0x0:
-        #print("if v{} == v{}, then skip".format(x, y))
+        print("if v{} == v{}, then skip".format(x, y))
         if reg[x] == reg[y]:
             pc += 2
     elif op == 0x9 and n == 0x0:
-        #print("if v{} != v{}, then skip".format(x, y))
+        print("if v{} != v{}, then skip".format(x, y))
         if reg[x] != reg[y]:
             pc += 2
     elif op == 0x6:
         # set
-        #print("set v%X" % x + " to 0x%X" % nn)
+        print("set v%X" % x + " to 0x%X" % nn)
         reg[x] = nn
     elif op == 0x7:
         # add
-        #print("add 0x%X" % nn + " to 0x%X" % reg[x], end=', ')
+        print("add 0x%X" % nn + " to 0x%X" % reg[x], end=', ')
         reg[x] = (reg[x] + nn) % 0x100
-        #print("sum is now 0x%X" % reg[x])
+        print("sum is now 0x%X" % reg[x])
     elif op == 0x8:
         # logic
         if n == 0x0:
             # set
-            #print("v{} is now set to the value in v{}".format(x, y))
+            print("v{} is now set to the value in v{}".format(x, y))
             reg[x] = reg[y]
         elif n == 0x1:
             # or
@@ -254,38 +242,38 @@ while True:
             # shift right
             if orig:
                 reg[x] = reg[y]
-            #print("shift v{} = 0x{:0X} right".format(x, reg[x]))
+            print("shift v{} = 0x{:0X} right".format(x, reg[x]))
             reg[0xF] = reg[x] & 0x1
             reg[x] = reg[x] >> 1
-            #print("v{} is now 0x{:0X}".format(x, reg[x]))
+            print("v{} is now 0x{:0X}".format(x, reg[x]))
         elif n == 0xE:
             # shift left
             if orig:
                 reg[x] = reg[y]
-            #print("shift v{} = 0x{:0X} left".format(x, reg[x]))
+            print("shift v{} = 0x{:0X} left".format(x, reg[x]))
             reg[0xF] = reg[x] >> 7
             reg[x] = reg[x] << 1
             reg[x] = reg[x] & 0xFF
-            #print("v{} is now 0x{:0X}".format(x, reg[x]))
+            print("v{} is now 0x{:0X}".format(x, reg[x]))
     elif op == 0xA:
         # set index
-        #print("set index to 0x{:03X} which points to 0x{:02X}".format(nnn, memory[nnn]))
+        print("set index to 0x{:03X} which points to 0x{:02X}".format(nnn, memory[nnn]))
         index = nnn
     elif op == 0xB:
         # offset jump
-        #print("jump to {:03X} + {:02X}".format(nnn, reg[0]))
+        print("jump to {:03X} + {:02X}".format(nnn, reg[0]))
         if orig:
             pc = nnn + reg[0]
         else:
             pc = nnn + reg[x]
     elif op == 0xC:
         # random
-        #print("make v{:01X} a random number &ed with {:02X}".format(x, nn))
+        print("make v{:01X} a random number &ed with {:02X}".format(x, nn))
         r = random.randint(0, nn)
         reg[x] = r & nn
     elif op == 0xD:
         # display
-        #print("display instruction")
+        print("display instruction")
         dx = reg[x] % 0x40
         dy = 0x20 - reg[y] % 0x20
         reg[0xF] = 0
@@ -295,14 +283,14 @@ while True:
             if dy > 0x1F:
                 break
             membyt = memory[ind]
-            ##print("membyt at index 0x%X" % ind + " is 0x%X" % membyt)
+            #print("membyt at index 0x%X" % ind + " is 0x%X" % membyt)
             dx = savx
             for j in range(0x8):
                 if dx > 0x3F:
                     break
                 dispbit = (display[dy] >> (0x3F - dx)) & 0x1
                 membit = (membyt >> (0x7 - j)) & 0x1
-                ##print("dispbit is 0x{:01x} and membit is 0x{:01x}".format(dispbit, membit))
+                #print("dispbit is 0x{:01x} and membit is 0x{:01x}".format(dispbit, membit))
                 if membit == 1:
                     display[dy] = display[dy] ^ (0x1 << (0x3F - dx))
                     if dispbit == 1:
@@ -315,38 +303,38 @@ while True:
     elif op == 0xE and nn == 0x9E:
         # skip if key is pressed
         ind = corr.index(reg[x])
-        #print(f"skip if \"{keys[ind]}\" is pressed")
+        print(f"skip if \"{keys[ind]}\" is pressed")
         if keyboard.is_pressed(keys[ind]):
-            #print(f"\"{keys[ind]}\" is indeed pressed")
+            print(f"\"{keys[ind]}\" is indeed pressed")
             pc += 2
     elif op == 0xE and nn == 0xA1:
         # skip if key is not pressed
         ind = corr.index(reg[x])
-        #print(f"skip if \"{keys[ind]}\" is not pressed")
+        print(f"skip if \"{keys[ind]}\" is not pressed")
         if not keyboard.is_pressed(keys[ind]):
-            #print(f"\"{keys[ind]}\" is indeed not pressed")
+            print(f"\"{keys[ind]}\" is indeed not pressed")
             pc += 2
     elif op == 0xF:
         # various instructions
         if nn == 0x07:
             # this and two next are timers
-            #print("v{:01X} is now 0x{:03X} from delay".format(x, reg[x]))
+            print("v{:01X} is now 0x{:03X} from delay".format(x, reg[x]))
             reg[x] = int(delay)
         elif nn == 0x15:
-            #print("delay is now 0x{:03X} from v{:01X}".format(reg[x], x))
+            print("delay is now 0x{:03X} from v{:01X}".format(reg[x], x))
             delay = reg[x]
         elif nn == 0x18:
-            #print("sound is now 0x{:03X} from v{:01X}".format(reg[x], x))
+            print("sound is now 0x{:03X} from v{:01X}".format(reg[x], x))
             sound = reg[x]
         elif nn == 0x1E:
             # add to index
-            #print("add v{:01X} = {:03X} to index".format(x, reg[x]))
+            print("add v{:01X} = {:03X} to index".format(x, reg[x]))
             index += reg[x]
             if index > 0xFFF:
                 index %= 0x1000
                 if amiga:
                     reg[0xF] = 1
-            #print("index is now {:03X}".format(index))
+            print("index is now {:03X}".format(index))
         elif nn == 0x0A:
             while True:
                 k = keyboard.read_key()
@@ -355,14 +343,14 @@ while True:
                 ind = keys.index(k)
                 break
             reg[x] = corr[ind]
-            #print("store 0x{:03X} in v{:01X} from \"{}\" being pressed".format(reg[x], x, corr[ind]))
+            print("store 0x{:03X} in v{:01X} from \"{}\" being pressed".format(reg[x], x, corr[ind]))
         elif nn == 0x29:
             # font character
             index = 0x50 + reg[x] * 0x5
-            #print("index is now at the font character \"{:01X}\"".format(reg[x]))
+            print("index is now at the font character \"{:01X}\"".format(reg[x]))
         elif nn == 0x33:
             # binary-coded decimal conversion
-            #print("put v{:01X} = {} into memory[{:03X}], in decimal".format(x, reg[x], index))
+            print("put v{:01X} = {} into memory[{:03X}], in decimal".format(x, reg[x], index))
             b = reg[x]
             memory[index + 2] = b % 10
             b = int(b / 10)
@@ -371,7 +359,7 @@ while True:
             memory[index] = b % 10
         elif nn == 0x55:
             # store memory
-            #print("store registers into memory at memory[{:03X}] until {:01X}".format(index, x))
+            print("store registers into memory at memory[{:03X}] until {:01X}".format(index, x))
             if inc:
                 for i in range(x + 1):
                     memory[index] = reg[i]
@@ -382,7 +370,7 @@ while True:
             pass
         elif nn == 0x65:
             # load memory
-            #print("load registers from memory from memory[{:03X}] until {:01X}".format(index, x))
+            print("load registers from memory from memory[{:03X}] until {:01X}".format(index, x))
             if inc:
                 for i in range(x + 1):
                     reg[i] = memory[index]
